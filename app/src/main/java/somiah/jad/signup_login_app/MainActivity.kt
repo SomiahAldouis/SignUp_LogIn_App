096ae668bc0c9e108135318d8fd6ffdfc3983b85
Somiah Aldouis <somiahjamal1997@gmail.com>
2020-12-21 19:20:57 +0300

    Initial commit

---
+++ app/src/main/java/somiah/jad/signup_login_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package somiah.jad.signup_login_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
